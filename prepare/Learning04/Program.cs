ab5a516bf1776e48ee78cbdc3e616c5ba430e369
Mhiztacsong <jgabriel13813@gmail.com>
2024-06-30 23:20:27 +0100

    learning activity 04 Homework

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Johnson Nsisong", "Surd");
+        Console.WriteLine(assignment.GetSummary() + "\n");
+
+        
+
+        MathAssignment mathAssignment = new MathAssignment("Victor Oshimen", "Quadratic equation", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(mathAssignment.GetHomeworkList() + "\n");
+
+        WritingAssignment writingAssignment = new WritingAssignment("Ibinabo MacDede", "African History", "The Causes of the Civil War in Nigeria by Ibinabo MacDede");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
